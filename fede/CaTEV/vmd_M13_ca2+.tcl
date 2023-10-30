faf4f4ff0cd9d5cfebd9350c1bdd0ee556dbd277
Federico Pozzani <fedepozz95@me.com>
2023-10-30 16:22:21 +0100

    added CaTEV MD analysis

---
+++ fede/CaTEV/vmd_M13_ca2+.tcl
@@ -0,0 +1,28 @@
+# M13_ca2+ systems visualization
+# Calcium
+mol modcolor 0 0 ColorID 0
+mol addrep 0
+mol modselect 1 0 name CAL
+mol modstyle 1 0 VDW 1.000000 12.000000
+mol modcolor 1 0 ColorID 4
+
+# Calcium binding sites EF1, EF2, EF3, EF4
+mol addrep 0
+mol modselect 2 0 resid 82 84 86 93
+mol modstyle 2 0 CPK 1.000000 0.300000 12.000000 12.000000
+mol modcolor 2 0 ColorID 7
+
+mol addrep 0
+mol modselect 3 0 resid 118 120 122 129
+mol modstyle 3 0 CPK 1.000000 0.300000 12.000000 12.000000
+mol modcolor 3 0 ColorID 9
+
+mol addrep 0
+mol modselect 4 0 resid 176 178 187
+mol modstyle 4 0 CPK 1.000000 0.300000 12.000000 12.000000
+mol modcolor 4 0 ColorID 1
+
+mol addrep 0
+mol modselect 5 0 resid 212 214 216 223
+mol modstyle 5 0 CPK 1.000000 0.300000 12.000000 12.000000
+mol modcolor 5 0 ColorID 3
